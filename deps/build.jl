82edb2bb2efb434bcaa9f0c327a5fe2974929c7a
Alinson S. Xavier <git@axavier.org>
2020-08-29 18:38:00 -0500

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,6 +1,21 @@
 using Conda
-ENV["PATH"] *= ";" * joinpath(Conda.ROOTENV, "Scripts")
-ENV["PATH"] *= ";" * joinpath(Conda.ROOTENV, "Library", "bin")
-pip = joinpath(Conda.ROOTENV, "Scripts", "pip")
-run(`$pip install miplearn==0.1.0`)
 
+function install_miplearn()
+    paths = [joinpath(Conda.ROOTENV, "Scripts"),
+             joinpath(Conda.ROOTENV, "Library", "bin"),
+             joinpath(Conda.ROOTENV, "bin")]
+
+    pip_found = false
+    for p in paths
+        if isfile("$p/pip3")
+            run(`$p/pip3 install miplearn==0.1.0`)
+            pip_found = true
+            break
+        end
+    end
+
+    pip_found || error("Could not find pip")
+end
+
+Conda.update()
+install_miplearn()
