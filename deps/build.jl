97b9fc9ad80e87e0a317218b2f362d899304f154
Alinson S Xavier <axavier@anl.gov>
2021-07-28 09:33:59 -0500

    Use MIPLearn 0.2.0.dev11

--- deps/build.jl
+++ deps/build.jl
@@ -5,7 +5,7 @@ function install_miplearn()
     Conda.update()
     pip = joinpath(dirname(pyimport("sys").executable), "pip")
     isfile(pip) || error("$pip: invalid path")
-    run(`$pip install miplearn==0.2.0.dev10`)
+    run(`$pip install miplearn==0.2.0.dev11`)
 end
 
 install_miplearn()
