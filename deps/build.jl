e5a2550c210163fae82987e38a53c129d0831fb3
Alinson S. Xavier <git@axavier.org>
2024-12-10 11:10:29 -0600

    Bump to MIPLearn 0.4.1

--- deps/build.jl
+++ deps/build.jl
@@ -5,7 +5,7 @@ function install_miplearn()
     Conda.update()
     pip = joinpath(dirname(pyimport("sys").executable), "pip")
     isfile(pip) || error("$pip: invalid path")
-    run(`$pip install miplearn==0.4.0`)
+    run(`$pip install miplearn==0.4.1`)
 end
 
 install_miplearn()
